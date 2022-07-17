db1bf02d9f5e4723038e1b4dd7cef2203d49b0d5
angeofrancis <109223158+angeofrancis@users.noreply.github.com>
2022-07-17 07:49:47 -0700

    Update and rename index1.php to index.php

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 require_once('table.php');
 
-$db = new Databse("127.0.0.1", "php_rest_api_db". "root", "");
+$db = new Database("127.0.0.1", "php_rest_api_db", "root", "");
 
 if($_SERVER["REQUEST_METHOD"]=="GET")
 {
